6a5203d80ea8447b85aa4a4ad0279214952eacde
Keri Southwood-Smith <9258461+KSouthwood@users.noreply.github.com>
2020-09-23 15:15:25 -0400

    [feat] complete Stage 3
    Main.main:
    - removed while loop, now just calls Main.readInput and waits for it to exit
    - moved summing loop to own method: Main.sumNumbers
    
    Main.readInput:
    - renamed boolean control variable to reflect change in flow
    - re-ordered the input checks to avoid certain possible errors. Checking for a blank line first avoids an IndexOutOfBoundsException when checking the first char in the String
    - moved the string splitting to the for-each loop. Simplifies the earlier checks. Also, isn't need anyway until this point.
    - add method calls to Main.parseCommand and Main.sumNumbers
    
    Main.parseCommand:
    - new method to handle parsing the commands (if valid)
    
    Main.sumNumbers:
    - new method to add all the numbers pushed onto the stack

--- calculator/Main.java
+++ calculator/Main.java
@@ -9,40 +9,61 @@ public class Main {
     static final Stack<Integer> stack = new Stack<>();
 
     public static void main(String[] args) {
-        while (readInput()) {
-            int sum = 0;
-            while (!stack.empty()) {
-                sum += stack.pop();
-            }
-            System.out.println(sum);
-        }
+        readInput();
         System.out.println("Bye!");
     }
 
-    private static boolean readInput() {
-        boolean inputError = true;
+    private static void readInput() {
+        boolean keepReading = true;
         Scanner scanner = new Scanner(System.in);
-        while (inputError) {
-            inputError = false;
+        while (keepReading) {
             stack.clear();
-            String[] input = scanner.nextLine().split("\\s");
-            if (input[0].equalsIgnoreCase("/exit")) {
-                return false;
-            }
-            if (input[0].isEmpty()) {
-                inputError = true;
+            String input = scanner.nextLine();
+
+            if (input.isBlank()) {
                 continue;
             }
-            for (var number : input) {
+
+            if (input.charAt(0) == '/') {   // checks to see if a command was entered (all commands begin with a forward slash)
+                if (parseCommand(input)) {  // process the command
+                    keepReading = false;    // return from the method if /exit was the command (only way the return value would be true)
+                }
+                continue;
+            }
+
+            for (var number : input.split("\\s")) {
                 if (number.matches(digits.pattern())) {
                     stack.push(Integer.parseInt(number));
                 } else {
-                    inputError = true;
                     System.out.printf("Invalid input: %s%n", number);
                     break;
                 }
             }
+
+            sumNumbers();
         }
-        return true;
+    }
+
+    private static boolean parseCommand(String command) {
+        switch (command.toLowerCase()) {
+            case "/exit":
+                return true;
+            case "/help":
+                System.out.println("Calculate the sum of integers (whole numbers) input on a single line separated by spaces.");
+                break;
+            default:
+                System.out.println("Unknown command: " + command);
+                break;
+        }
+
+        return false;
+    }
+
+    private static void sumNumbers() {
+        int sum = 0;
+        while (!stack.empty()) {
+            sum += stack.pop();
+        }
+        System.out.println(sum);
     }
 }
