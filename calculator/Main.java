acc0cc87ab04ed9aa8bcf78d5a593e74720dc471
Keri Southwood-Smith <9258461+KSouthwood@users.noreply.github.com>
2020-09-23 12:07:37 -0400

    [feat] complete Stage 2
    - Main.readInput now returns boolean indicating if "/exit" was entered to quit the program
    - Main.main refactored to use a while loop using Main.readInput return value to control looping
    - Add two checks in Main.readInput looking for "/exit" command and input of a blank line

--- calculator/Main.java
+++ calculator/Main.java
@@ -9,21 +9,30 @@ public class Main {
     static final Stack<Integer> stack = new Stack<>();
 
     public static void main(String[] args) {
-        readInput();
-        int sum = 0;
-        while (!stack.empty()) {
-            sum += stack.pop();
+        while (readInput()) {
+            int sum = 0;
+            while (!stack.empty()) {
+                sum += stack.pop();
+            }
+            System.out.println(sum);
         }
-        System.out.println(sum);
+        System.out.println("Bye!");
     }
 
-    private static void readInput() {
+    private static boolean readInput() {
         boolean inputError = true;
         Scanner scanner = new Scanner(System.in);
         while (inputError) {
             inputError = false;
             stack.clear();
             String[] input = scanner.nextLine().split("\\s");
+            if (input[0].equalsIgnoreCase("/exit")) {
+                return false;
+            }
+            if (input[0].isEmpty()) {
+                inputError = true;
+                continue;
+            }
             for (var number : input) {
                 if (number.matches(digits.pattern())) {
                     stack.push(Integer.parseInt(number));
@@ -34,5 +43,6 @@ public class Main {
                 }
             }
         }
+        return true;
     }
 }
